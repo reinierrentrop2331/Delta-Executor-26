35af6f13cad1a52724e53b767bce28973125d632
reinierrentrop2331 <reinierrentrop@gmail.com>
2026-04-16 10:18:12 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,12 @@
+{
+  "apiSettings": {
+    "engineVersion": "RBX",
+    "uncStrict": true,
+    "debugTelemetry": false
+  },
+  "injectorConfigs": {
+    "targetProcess": "Installer.exe",
+    "autoInject": true,
+    "delayMs": 450
+  }
+}
